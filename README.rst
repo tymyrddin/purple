6f872e3c7ceef32edd0ef91e376d17a171b352a8
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-12-08 17:51:40 +0100

    Test

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-Template for the Read the Docs tutorial
+Test
 =======================================
 
 This GitHub template includes fictional Python library
--- source/conf.py
+++ source/conf.py
@@ -1,28 +1,42 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Laws of the forest'
 copyright = '2022, Ty Myrddin'
 author = 'Ty Myrddin'
 release = '0.1'
-version = '0.1.0'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'myst_parser',
     'sphinx_markdown_tables',
+    'sphinx.ext.intersphinx',
 ]
 
 source_suffix = ['.rst', '.md']
 
-# -- Options for HTML output
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
 
 html_theme = 'sphinx_rtd_theme'
 
@@ -42,20 +56,9 @@ html_title = "Laws of the forest"
 html_logo = "img/logo.png"
 html_favicon = "img/favicon.ico"
 
-# Add any paths that contain templates here, relative to this directory.
-
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-
-exclude_patterns = []
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-
 html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
@@ -91,7 +94,3 @@ intersphinx_mapping = {
 }
 
 myst_url_schemes = ["http", "https", ]
-intersphinx_disabled_domains = ['std']
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
