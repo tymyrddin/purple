f33d60f2f27daeb94d60dec23f5ec1fab8f13523
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2025-05-11 22:37:24 +0200

    Topnav links

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ html_theme_options = {
     ],
 }
 
-html_title = "Purple Team"
+html_title = "Purple team"
 html_logo = "img/logo.png"
 html_favicon = "img/favicon.ico"
 html_static_path = ['_static']
